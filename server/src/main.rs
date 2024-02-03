f817c102722d4a868cbb22135c06245ec023d1f7
lucca <luccagugescu@gmail.com>
2024-02-03 15:40:37 +0100

    add html response for error and page

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,6 @@
-use std::io::Read;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
 
@@ -14,7 +16,20 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-    // read request header
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer));
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(
+        get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "page.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let response = format!("{}{}", status_line, contents)
+        ;
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
