c98399ef4e4405634d8fd8b0bfced0d57bfeb647
lucca <luccagugescu@gmail.com>
2024-02-03 16:41:04 +0100

    refactor code

--- server/src/bin/src/main.rs
+++ server/src/bin/src/main.rs
@@ -2,14 +2,19 @@ use std::fs::File;
 use std::io::{Read, Write};
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
+use server::ThreadPool;
 
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").
-        unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -17,10 +22,12 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-
     let get = b"GET / HTTP/1.1\r\n";
     let (status_line, filename) = if buffer.starts_with(
         get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "page.html")
+    } else if buffer.starts_with(b"sleep") {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "page.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
